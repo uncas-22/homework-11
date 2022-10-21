0c9f67b3f00cdc9a28ea9ee1fbcb544af1a0b50c
Alex <seredin-alex@mail.ru>
2022-10-21 06:54:25 +0300

    Урок 11. Строки Задача 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Урок 11. Строки. Задача 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
 
     }
 }
