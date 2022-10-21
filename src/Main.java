649cd83252aaa43a662ebe65d7c4f7ba0e922c90
Alex <seredin-alex@mail.ru>
2022-10-21 06:56:04 +0300

    Урок 11. Строки. Задача 3

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,13 @@ public class Main {
         String s = fullName;
         String s1 = s.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + s1);
+        System.out.println("Урок 11. Строки. Задача 3");
+        String fullName2 = "Иванов Семён Семёнович";
+        String oldString = fullName2;
+        String newString =  oldString.replace('ё', 'е');
+
+        System.out.println(oldString);
+        System.out.println(newString);
 
     }
 }
